c7c4919addc89678a04b619284f11125a20d3fd6
Daniel Pat <danielpat6@gmail.com>
2022-07-29 21:01:48 -0500

    add method for inital state

--- ruby2d/app.rb
+++ ruby2d/app.rb
@@ -1,4 +1,6 @@
 require_relative 'view/ruby2d'
 
 view = View::Ruby2dView.new
+
+initial_state = Model::initial_state
 view.render(nil)
--- ruby2d/model/state.rb
+++ ruby2d/model/state.rb
@@ -1,4 +1,5 @@
 module Model
+    
     class Coord
         attr_accessor :row, :col
     end
@@ -17,4 +18,16 @@ module Model
     class State
         attr_accessor :snake, :grid, :food
     end
+
+    def self.initial_state
+        Model::State.new(
+            snake: Model::Snake.new([
+                Model::Coord.new(1,1)
+                Model::Coord.new(0,1)
+            ]),
+            grid: Model::Grid.new(8,12),
+            food: Model::Food.new(4,4)
+        )
+    end
+
 end
