b460cbdcab9db84f9b300ee65937a94ac251bbb4
Daniel Pat <danielpat6@gmail.com>
2022-07-29 21:22:19 -0500

    render food and snake

--- ruby2d/app.rb
+++ ruby2d/app.rb
@@ -1,6 +1,7 @@
 require_relative 'view/ruby2d'
-
+require_relative 'model/state'
 view = View::Ruby2dView.new
 
 initial_state = Model::initial_state
-view.render(nil)
+
+view.render(initial_state)
--- ruby2d/view/ruby2d.rb
+++ ruby2d/view/ruby2d.rb
@@ -2,11 +2,42 @@ require 'ruby2d'
 module View
     class Ruby2dView
         def initialize
+            @pixe_size = 50
         end
         def render(state)
             extend Ruby2D::DSL
-            set title: "Snake", width: 600, height: 400
+            set(
+                title: "Snake",
+                width: @pixe_size * state.grid.cols,
+                height: @pixe_size * state.grid.rows
+            )
+            render_snake(state)
+            render_food(state)
             show
+        end
+        
+        private
+        def render_food(state)
+            extend Ruby2D::DSL
+            food = state.food
+            Square.new(
+                x: food.col * @pixe_size, y: food.row * @pixe_size,
+                size: @pixe_size,
+                color: 'yellow',
+            )
+            
+        end
+        def render_snake(state)
+            extend Ruby2D::DSL
+            snake = state.snake
+            snake.positions.each do |position| 
+                Square.new(
+                    x: position.col * @pixe_size, y: position.row * @pixe_size,
+                    size: @pixe_size,
+                    color: 'green',
+                )
+            end
+            
         end
     end
 end
