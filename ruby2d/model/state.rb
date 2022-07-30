6e74e7ab96b3198c2f6c2262c9ae3fef95110a5a
Daniel Pat <danielpat6@gmail.com>
2022-07-29 21:14:18 -0500

    fix classes

--- ruby2d/model/state.rb
+++ ruby2d/model/state.rb
@@ -2,31 +2,50 @@ module Model
     
     class Coord
         attr_accessor :row, :col
+        def initialize(row, col)
+            @row = row
+            @col = col
+        end
     end
 
     class Food < Coord
+        def initialize(row, col)
+            super
+        end
     end
 
     class Snake
         attr_accessor :positions
+        def initialize(positions)
+            @positions = positions
+        end
     end
 
     class Grid
         attr_accessor :rows, :cols
+        def initialize(rows, cols)
+            @rows = rows
+            @cols = cols
+        end
     end
 
     class State
         attr_accessor :snake, :grid, :food
+        def initialize(snake, grid, food)
+            @snake = snake
+            @grid = grid
+            @food = food
+        end
     end
 
     def self.initial_state
         Model::State.new(
-            snake: Model::Snake.new([
-                Model::Coord.new(1,1)
+            Model::Snake.new([
+                Model::Coord.new(1,1),
                 Model::Coord.new(0,1)
             ]),
-            grid: Model::Grid.new(8,12),
-            food: Model::Food.new(4,4)
+            Model::Grid.new(8,12),
+            Model::Food.new(4,4)
         )
     end
 
